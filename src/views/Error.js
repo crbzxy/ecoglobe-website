5e5713a564c75b3754832f9daff407bfc37935af
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2022-06-16 00:50:24 -0500

    ajuste 404

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
     <div className=" contenedor Error">
-      <h1>Error</h1>
+
+      <h1>Pagina no encontrada</h1>
+      <Link className="btn-primario" to="/">Volver al inicio</Link>
     </div>
   );
 }
